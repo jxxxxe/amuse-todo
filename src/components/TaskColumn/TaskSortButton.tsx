391330a57e8caa39d058f7eae828c8735c119476
J <68155263+jxxxxe@users.noreply.github.com>
2025-03-14 14:47:07 +0900

    Feat: 카드 정렬 기능 추가

--- src/components/TaskColumn/TaskSortButton.tsx
+++ src/components/TaskColumn/TaskSortButton.tsx
@@ -1,13 +1,38 @@
 import { useState } from "react";
 import useClickAway from "../../hooks/useClickAway";
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/16/solid";
+import useTaskStore from "../../stores/useTaskStore";
 
-const TaskSortButton = () => {
+interface TaskSortButtonProps {
+  columnId: number;
+}
+
+const TaskSortButton = ({ columnId }: TaskSortButtonProps) => {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const sortRef = useClickAway<HTMLDivElement>(() => setIsPopoverOpen(false));
+  const sortNameList = ["이름순", "중요도순", "시작일순", "종료일순"];
+
+  const {
+    sortTaskByName,
+    sortTaskByPriority,
+    sortTaskByStartDate,
+    sortTaskByEndDate,
+  } = useTaskStore();
 
   const openPopover = () => {
     setIsPopoverOpen(true);
+  };
+
+  const sortList = (index: number) => {
+    if (index === 0) {
+      sortTaskByName(columnId);
+    } else if (index === 1) {
+      sortTaskByPriority(columnId);
+    } else if (index === 2) {
+      sortTaskByStartDate(columnId);
+    } else if (index === 3) {
+      sortTaskByEndDate(columnId);
+    }
   };
 
   return (
@@ -18,12 +43,13 @@ const TaskSortButton = () => {
       {isPopoverOpen && (
         <div
           ref={sortRef}
-          className="absolute *:text-start w-19 top-full *:hover:bg-gray-100 divide-y divide-gray-100 flex *:p-2 flex-col rounded border border-gray-300 bg-white"
+          className={`absolute *:text-start *: w-19 top-full *:hover:bg-gray-100 divide-y divide-gray-100 flex *:p-2 flex-col rounded border border-gray-300 bg-white`}
         >
-          <button>이름순</button>
-          <button>중요도순</button>
-          <button>시작일순</button>
-          <button>종료일순</button>
+          {sortNameList.map((name, index) => (
+            <button key={index} onClick={() => sortList(index)}>
+              {name}
+            </button>
+          ))}
         </div>
       )}
     </div>
--- src/components/TaskColumn/index.tsx
+++ src/components/TaskColumn/index.tsx
@@ -1,26 +1,48 @@
-import { ITask } from "../../types/task";
+import { useState } from "react";
+import { ITask, TaskStateType } from "../../types/task";
 import TaskAddButton from "./TaskAddButton";
 import TaskCard from "./TaskCard";
 import TaskSortButton from "./TaskSortButton";
+import TaskCardEditor from "../TaskCardEditor";
+import useClickAway from "../../hooks/useClickAway";
 
 interface ColumnProps {
-  stateTitle: string;
-  cardList: ITask[];
+  columnId: number;
+  stateTitle: TaskStateType;
+  initialCardList: ITask[];
 }
 
-const StateColumn = ({ stateTitle, cardList }: ColumnProps) => {
+const StateColumn = ({
+  columnId,
+  stateTitle,
+  initialCardList,
+}: ColumnProps) => {
+  const [cardList, setCardList] = useState(initialCardList);
+  const [isCardEditing, setIsCardEditing] = useState(false);
+
+  const onTaskAddButtonClick = () => {
+    setIsCardEditing(true);
+  };
+
+  const onEditorClickAway = () => {
+    setIsCardEditing(false);
+  };
+
+  const editorRef = useClickAway<HTMLDivElement>(onEditorClickAway);
+
   return (
-    <div className="flex flex-col gap-5 mt-5">
+    <div className="flex flex-col gap-5 mt-5 w-full">
       <div className="flex justify-between text-gray-500 px-1">
         <span className="text-sm">{stateTitle}</span>
-        <TaskSortButton />
+        <TaskSortButton columnId={columnId} />
       </div>
       <div className="flex flex-col gap-5">
         {cardList.map((card, index) => (
-          <TaskCard key={`card-${stateTitle}-${index}`} card={card} />
+          <TaskCard key={`card-${stateTitle}-${index}`} taskInfo={card} />
         ))}
+        {isCardEditing && <TaskCardEditor ref={editorRef} />}
       </div>
-      <TaskAddButton />
+      <TaskAddButton onClick={onTaskAddButtonClick} />
     </div>
   );
 };

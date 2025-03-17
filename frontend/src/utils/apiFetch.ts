5fc82ef3e3b7e2d0d9d90b69e80fbd0cd6475d91
J <68155263+jxxxxe@users.noreply.github.com>
2025-03-17 13:21:23 +0900

    vite_api 테스트

--- frontend/src/utils/apiFetch.ts
+++ frontend/src/utils/apiFetch.ts
@@ -1,6 +1,7 @@
 const { VITE_API_BASE_URL } = import.meta.env;
 
 const apiFetch = async (requestUrl: string, options?: RequestInit) => {
+  console.log(`${VITE_API_BASE_URL}`);
   const res = await fetch(`${VITE_API_BASE_URL}/${requestUrl}`, options);
   return res.json();
 };
